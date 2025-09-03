09b8d1ee91c2f9af293f558c25736466adb1105b
DXICIDE <Rostaknc@seznam.cz>
2025-09-03 18:49:23 +0200

    added deleting chirps by user

---
+++ handlerDeleteChirp.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/DXICIDE/remote_server_go/internal/auth"
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) handlerDeleteChirp(w http.ResponseWriter, r *http.Request) {
+	PathValue := r.PathValue("chirpID")
+	id, err := uuid.Parse(PathValue)
+	if err != nil {
+		log.Printf("Error getting ID : %s", err)
+		respondWithError(w, http.StatusBadRequest, "invalid chirp id")
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		log.Printf("Error getting Token: %s", err)
+		w.WriteHeader(401)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.secret)
+	if err != nil {
+		log.Printf("Error Validating: %s", err)
+		w.WriteHeader(401)
+		return
+	}
+
+	chirp, err := cfg.db.GetChirp(r.Context(), id)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "chirp not found")
+		return
+	}
+
+	if userID != chirp.UserID {
+		respondWithError(w, http.StatusForbidden, "chirp not found")
+		return
+	}
+
+	err = cfg.db.DeleteChirp(r.Context(), chirp.ID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "chirp not found")
+		return
+	}
+
+	respondWithJSON(w, 204, "")
+}
